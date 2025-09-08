e1edbb17cbe28cd5624db9bc87587e3dc5b0d91e
karthidev-sudo <karthiknainar2000@gmail.com>
2025-09-08 20:33:07 +0530

    Fix wildcard route for production deployment

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,8 @@ app.use("/api/analytics", analyticsRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("/*", (req, res) => {
+  // Fallback route for React SPA
+  app.get("/:path(*)", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
